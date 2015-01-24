41b3ca4bed4c3b7698dbf157f7454541b5e53ce2
corynissen <corynissen@gmail.com>
2015-01-24 16:41:32 -0600

    documentation cleanup, argument checking.

--- R/login.R
+++ R/login.R
@@ -1,16 +1,19 @@
 #' Login to fitbit.com and get cookie
 #'
-#' Enter a string and have latitude and longitude returned using the HERE API
+#' Get the login cookie after login at www.fitbit.com
 #' @param email Email address used to login to fitbit.com
 #' @param password Password used to login to fitbit.com
 #' @keywords login
 #' @export
 #' @examples
 #' \dontrun{
-#' login(email="corynissen<at>gmail.com", password="mypasswordhere)
+#' cookie <- login(email="corynissen<at>gmail.com", password="mypasswordhere")
 #' }
 #' login
 login <- function(email, password){
+  if(!is.character(email)){stop("email must be a character string")}
+  if(!is.character(password)){stop("password must be a character string")}
+
   url <- "https://www.fitbit.com/login"
   headers <- list("Host" = "www.fitbit.com",
                   "Connection" = "keep-alive",
@@ -25,9 +28,9 @@ login <- function(email, password){
                   "Accept-Language" = "en-US,en;q=0.8")
   body <- list("email"=email, "password"=password, "rememberMe"="true",
                "login"="Log In")
-  
+
   a <- httr::POST(url, headers=headers, body=body)
   cookie <- a$cookies$u
+  if(is.null(cookie)){stop("login failed")}
   return(cookie)
 }
-  
