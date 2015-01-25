840ba769d8e247c49cafaec207dd692104fbc105
corynissen <corynissen@gmail.com>
2015-01-25 15:15:46 -0600

    formatting

--- R/get_sleep_data.R
+++ R/get_sleep_data.R
@@ -1,6 +1,6 @@
-#' Get sleep data from fitbit using cookie returned from login function
-#'
 #' Get sleep data from fitbit.com
+#'
+#' Get sleep data from fitbit using cookie returned from login function
 #' @param cookie Cookie returned after login, specifically the "u" cookie
 #' @param start_date Date in YYYY-MM-DD format
 #' @param end_date Date in YYYY-MM-DD format
