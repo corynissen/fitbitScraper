2ca554adf91fb5f912baa921e316bf1b8c1c430b
Nissen <60018847@hospira.corp>
2017-04-13 10:06:00 -0500

    Added minutesSedentary and fixed bug in getTimeInheartRateZonesPerDay dataframe

--- R/get_daily_data.R
+++ R/get_daily_data.R
@@ -2,7 +2,7 @@
 #'
 #' Get daily data from fitbit using cookie returned from login function
 #' @param cookie Cookie returned after login, specifically the "u" cookie
-#' @param what What data you wish to be returned. Options include "steps", "distance", "floors", "minutesVery", "caloriesBurnedVsIntake", "getTimeInHeartRateZonesPerDay", "getRestingHeartRateData"
+#' @param what What data you wish to be returned. Options include "steps", "distance", "floors", "minutesVery", "caloriesBurnedVsIntake", "minutesSedentary", "getTimeInHeartRateZonesPerDay", "getRestingHeartRateData"
 #' @param start_date Date in YYYY-MM-DD format
 #' @param end_date Date in YYYY-MM-DD format
 #' @keywords data
@@ -20,11 +20,19 @@ get_daily_data <- function(cookie, what="steps", start_date, end_date){
   if(!is.character(what)){stop("what must be a character string")}
   if(!is.character(start_date)){stop("start_date must be a character string")}
   if(!is.character(end_date)){stop("end_date must be a character string")}
-  if(!grepl("[0-9]{4}-[0-9]{2}-[0-9]{2}", start_date)){stop('start_date must have format "YYYY-MM-DD"')}
-  if(!grepl("[0-9]{4}-[0-9]{2}-[0-9]{2}", end_date)){stop('end_date must have format "YYYY-MM-DD"')}
-  if(!what %in% c("steps", "distance", "floors", "minutesVery", "caloriesBurnedVsIntake",
-                  "getTimeInHeartRateZonesPerDay", "getRestingHeartRateData")){
-    stop('what must be one of "steps", "distance", "floors", "minutesVery", "caloriesBurnedVsIntake", "getTimeInHeartRateZonesPerDay", "getRestingHeartRateData"')
+  if(!grepl("[0-9]{4}-[0-9]{2}-[0-9]{2}", start_date)){
+    stop('start_date must have format "YYYY-MM-DD"')
+  }
+  if(!grepl("[0-9]{4}-[0-9]{2}-[0-9]{2}", end_date)){
+    stop('end_date must have format "YYYY-MM-DD"')
+  }
+  if(!what %in% c("steps", "distance", "floors", "minutesVery", 
+                  "caloriesBurnedVsIntake", "minutesSedentary",
+                  "getTimeInHeartRateZonesPerDay", 
+                  "getRestingHeartRateData")){
+    stop('what must be one of "steps", "distance", "floors", "minutesVery", 
+         "caloriesBurnedVsIntake", "minutesSedentary", 
+         "getTimeInHeartRateZonesPerDay", "getRestingHeartRateData"')
   }
 
   if(what %in% c("getTimeInHeartRateZonesPerDay", "getRestingHeartRateData")){
@@ -56,9 +64,18 @@ get_daily_data <- function(cookie, what="steps", start_date, end_date){
   dat_list <- jsonlite::fromJSON(dat_string)
 
   if(what=="getTimeInHeartRateZonesPerDay"){
+    if(length(dat_list$value)==0){
+      stop("No getTimeInHeartRateZonesPerDay available")
+    }
     df <- cbind(dat_list$dateTime, dat_list$value, stringsAsFactors=FALSE)
-    names(df)[1:3] <- c("time", "zone1", "zone2", "zone3")
+    names(df)[1] <- "time"
+    names(df)[names(df)=="IN_DEFAULT_ZONE_1"] <- "zone1"
+    names(df)[names(df)=="IN_DEFAULT_ZONE_2"] <- "zone2"
+    names(df)[names(df)=="IN_DEFAULT_ZONE_3"] <- "zone3"
   }else if(what=="getRestingHeartRateData"){
+    if(length(dat_list$dataPoints)==0){
+      stop("No getRestingHeartRateData available")
+    }
     df <- dat_list$dataPoints
     df <- df[, c("date", "value")]
     names(df)[1:2] <- c("time", "restingHeartRate")
